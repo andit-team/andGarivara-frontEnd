f69c03087937b61caad908a054c05acbdf22cda9
Suraiya@AndIT <webguru071@gmail.com>
2020-06-09 15:36:07 +0600

    Rides Page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,31 +14,6 @@
 
 /// MAIN MENU SCRIPT START
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
-
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
@@ -63,6 +38,9 @@
   // mobile Menu area
 
   // MAIN MENU SCRIPT END
+
+  // Tooltip JS
+  $('[data-toggle="tooltip"]').tooltip();
 
   //----- Initialize WOW JS ------
   new WOW().init();
