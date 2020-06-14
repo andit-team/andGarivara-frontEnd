0d11ea8d6d7ac4205fd5c2e5c5eaf23ccfae71b1
Suraiya@AndIT <webguru071@gmail.com>
2020-06-14 18:02:19 +0600

    Car Leasing Page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,6 +39,33 @@
 
   // MAIN MENU SCRIPT END
 
+    // Sticky Menu Header JS Start
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '.sticky-menu-header',
+        offset: 54
+    });
+    // Sticky Menu Header JS End
+
     //Accordion Box
     if($('.accordion-box').length){
         $(".accordion-box").on('click', '.acc-btn', function() {
