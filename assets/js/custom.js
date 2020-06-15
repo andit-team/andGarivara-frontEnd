4454b5f9476d18f83a1ac3cfc61614416795390c
Suraiya@AndIT <webguru071@gmail.com>
2020-06-15 15:48:20 +0600

    Car Leasing Page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,47 +26,60 @@
 
   // MAIN MENU SCRIPT END
 
-    // Sticky Menu Header JS Start
+  // Sticky Menu Header JS Start
 
-    // Collapse Navbar
-    // var navbarCollapse = function() {
-    //     if ($("#mainNav").offset().top > 100) {
-    //         $("#mainNav").addClass("navbar-shrink");
-    //     } else {
-    //         $("#mainNav").removeClass("navbar-shrink");
-    //     }
-    // };
-    // Collapse now if page is not at top
-    // navbarCollapse();
+    // Add active class to the current button (highlight it)
+    // var header = document.getElementById("new-sticky-menu");
+    // var btns = header.getElementsByClassName("js-scroll-trigger");
+    // for (var i = 0; i < btns.length; i++) {
+    //     btns[i].addEventListener("click", function() {
+    //         var current = document.getElementsByClassName("active");
+    //         current[0].className = current[0].className.replace(" active", "");
+    //         this.className += " active";
+    //     });
+    // }
 
-    // Collapse the navbar when page is scrolled
-    // $(window).scroll(navbarCollapse);
+    // // Add smooth scrolling to all links
+    // $("a").on('click', function(event) {
+    //
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "") {
+    //         // Prevent default anchor click behavior
+    //         event.preventDefault();
+    //
+    //         // Store hash
+    //         var hash = this.hash;
+    //
+    //         // Using jQuery's animate() method to add smooth page scroll
+    //         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //         }, 800, function(){
+    //
+    //             // Add hash (#) to URL when done scrolling (default click behavior)
+    //             window.location.hash = hash;
+    //         });
+    //     } // End if
+    // });
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 48)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
+
+
+    let scroll_link = $('.sticky-menu-header ul li a.js-scroll-trigger');
+
+    //smooth scrolling -----------------------
+    scroll_link.click(function(e){
+        e.preventDefault();
+        let url = $('body').find($(this).attr('href')).offset().top;
+        $('html, body').animate({
+            scrollTop : url
+        },700);
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        return false;
+
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-        $('.sticky-menu-header').collapse('hide');
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#new-sticky-menu',
-        offset: 54
-    });
-    // Sticky Menu Header JS End
+  // Sticky Menu Header JS End
 
     //Accordion Box
     if($('.accordion-box').length){
