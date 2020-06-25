5a0d296c674e2bc027d59c15ae9bb80960e523f0
Suraiya@AndIT <webguru071@gmail.com>
2020-06-25 16:15:13 +0600

    New Bangla Content Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -114,6 +114,10 @@
     $('#datepicker2').datepicker({
         uiLibrary: 'bootstrap4'
     });
+
+    // Ride Alert Agent New Calendar JS
+    $('#datepicker3').datepicker({ calendarWeeks: true, modal: false, footer: true });
+
     // Datepicker JS End
 
     // Select Time Picker JS Start
@@ -146,70 +150,13 @@
     // Range Slider JS End
 
     // Agents New Step Form JS Start
-    var current_fs, next_fs, previous_fs; //fieldsets
-    var opacity;
-
-    $(".next").click(function(){
-
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
-
-//Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-//show the next fieldset
-        next_fs.show();
-//hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-            step: function(now) {
-// for making fielset appear animation
-                opacity = 1 - now;
-
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
-                });
-                next_fs.css({'opacity': opacity});
-            },
-            duration: 600
-        });
+    $('.btnNext').click(function() {
+        $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
     });
 
-    $(".previous").click(function(){
-
-        current_fs = $(this).parent();
-        previous_fs = $(this).parent().prev();
-
-//Remove class active
-        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-//show the previous fieldset
-        previous_fs.show();
-
-//hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-            step: function(now) {
-// for making fielset appear animation
-                opacity = 1 - now;
-
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
-                });
-                previous_fs.css({'opacity': opacity});
-            },
-            duration: 600
-        });
+    $('.btnPrevious').click(function() {
+        $('.nav-tabs .active').parent().prev('li').find('a').trigger('click');
     });
-
-    $('.radio-group .radio').click(function(){
-        $(this).parent().find('.radio').removeClass('selected');
-        $(this).addClass('selected');
-    });
-
-    $(".submit").click(function(){
-        return false;
-    })
     // Agents New Step Form JS End
 
   //----- Initialize WOW JS ------
